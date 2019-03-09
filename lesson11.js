c587c0b23935dc6703a59cb72487f979e594bba7
Peter Horton <phorton@softstackfactory.com>
2019-03-09 05:26:41 +0000

    3.8.19-1

--- lesson11.js
+++ lesson11.js
@@ -1,6 +1,10 @@
 //Desired result Print out all names except the name passed into 
 //the nameFilter function 
 
+//Sup Fucker
+//You gonna fuck on me?
+//So long, gay boys!
+
 var people = [
     { name: "John", age: 55 },
     { name: "Roger", age: 44 },
@@ -8,14 +12,20 @@ var people = [
     ]
 
 var nameFilter = function(filteredName) {
-    var results = []
+    var results = [] 
     for (var i = 0; i < people.length; i++) {
         let name = people[i].name
-        if (name != filteredName) {
-            results.push(name)
+        if (name != filteredName) { // <---- if name not fileteredName
+            results.push(name) // <---- save that shit here!!!!!
         }
-    }
+}
+    return results
 }
 
-var filteredNames = nameFilter("Betty")
-console.log(filteredNames)
+
+// This shit is wrong...
+// var filteredNames = nameFilter("Betty")
+
+
+// log this
+console.log(nameFilter("Betty"))
